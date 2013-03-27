76c65170dfd6cf5219cea30de937670d9c77abf9
Larry Sprock <larry.sprock@revolutionprep.com>
2013-03-27 11:36:52 -0700

    Add Webex config sample and load it

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,14 @@
 # encoding: utf-8
 require 'bundler/setup'
 require 'webex_client'
+require 'vcr'
+
+WEBEX_CONFIG = YAML.load(File.open(File.expand_path '../webex_config.yml', __FILE__).read)
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.hook_into :fakeweb
+end
 
 RSpec.configure do |c|
   c.mock_with :mocha
