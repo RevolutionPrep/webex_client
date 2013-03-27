42258debda026b3dd8907d54cd44813c94caacd5
Larry Sprock <larry.sprock@revolutionprep.com>
2013-03-27 13:33:19 -0700

    Use vcr

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,10 +6,19 @@ require 'vcr'
 WEBEX_CONFIG = YAML.load(File.open(File.expand_path '../webex_config.yml', __FILE__).read)
 
 VCR.configure do |c|
+  c.filter_sensitive_data("<PW>") { WEBEX_CONFIG['ADMIN_PASSWORD'] }
+  c.filter_sensitive_data("<WID>") { WEBEX_CONFIG['ADMIN_USERNAME'] }
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.hook_into :fakeweb
 end
 
 RSpec.configure do |c|
   c.mock_with :mocha
+
+  c.before(:each) do
+    WebexClient.instance_variable_set :@base_uri, nil
+    WebexClient.instance_variable_set :@meeting_password, nil
+    WebexClient.instance_variable_set :@admin_password, nil
+    WebexClient.instance_variable_set :@admin_username, nil
+  end
 end
